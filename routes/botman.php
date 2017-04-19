100de8da4bbc10d409c9d8b60aae4efec0fc06f8
Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
2017-04-19 12:16:42 -0300

    Create simple chatbot rule style

--- routes/botman.php
+++ routes/botman.php
@@ -3,7 +3,29 @@ use App\Http\Controllers\BotManController;
 // Don't use the Facade in here to support the RTM API too :)
 $botman = resolve('botman');
 
-$botman->hears('test', function($bot){
-    $bot->reply('hello!');
-});
 $botman->hears('Start conversation', BotManController::class.'@startConversation');
+
+// $botman->hears('/calc', function($bot){
+//     $bot->reply('hello!');
+// });
+
+$botman->fallback(function($bot) {
+    /*
+    {
+      +"version": "v1.5.28"
+      +"date1": "2015-10-22T00:00:00.000Z"
+      +"date2": "2017-04-19T00:00:00.000Z"
+      +"vLeoToday": true
+    }
+    */
+    $cadeOLeoVer = json_decode(
+        file_get_contents(
+          env('URL_CADE_O_LEO_VER')
+    ));
+
+
+
+    $bot->reply(
+        'Olá, ' . $bot->getUser()->getFirstName() . ', hoje a versão do Léo é: ' . $cadeOLeoVer->version . '. Quer saber a sua? Acessa lá: https://cadeoleo.github.io/'
+    );
+});
